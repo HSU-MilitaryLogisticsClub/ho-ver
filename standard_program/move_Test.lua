e79b2ae7281a91ba87ab2735b312184543037e29
tubo-tubo <1130131@stu.hus.ac.jp>
2015-07-21 17:45:39 +0900

    correction

--- standard_program/move_Test.lua
+++ standard_program/move_Test.lua
@@ -1,20 +1,20 @@
 require "move"
 
 function TestGpio()
-	LuaGpio:Forward(10)
-	LuaGpio:Stop()
+	LuaGpio.Forward(10)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
-	LuaGpio:Back(10)
-	LuaGpio:Stop()
+	LuaGpio.Back(10)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
-	LuaGpio:Left(10)
-	LuaGpio:Stop()
+	LuaGpio.Left(10)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
-	LuaGpio:Right(10)
-	LuaGpio:Stop()
+	LuaGpio.Right(10)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
 end
