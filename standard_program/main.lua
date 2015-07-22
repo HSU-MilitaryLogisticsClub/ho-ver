24db4cca26e7eff431faca42d00faaea7f8c0afc
tubo-tubo <1130131@stu.hus.ac.jp>
2015-07-22 18:28:15 +0900

    second

--- standard_program/main.lua
+++ standard_program/main.lua
@@ -1,12 +1,25 @@
 require "move"
 require "receive_shell"
 
-main = {}
+main = {
+	LuaGpio:ReadyGpio()
+	get = {}
+}
 
 function main.Catch(self)
 	repeat
-		LuaGpio:Left(6)
-		get = Receive:ReadShell()
-		until 
+		while true do
+			LuaGpio:Left(6)
+			self.get = Receive:ReadShell()
+		
+			if get[1]>0, get[2]>0, get[3]>0 do --
+				LuaGpio:Stop()
+				break
+			end
+		end
 
+		LuaGpio:Forward(5)
+		LuaGpio:Stop()
+
+		until get[1]>0, get[2]>0, get[3]>0
 end
