4f9961f06e15721e2f8ed9464a34179985b9bf73
tubo-tubo <1130131@stu.hus.ac.jp>
2015-08-04 15:50:23 +0900

    proto

--- standard_program/main.lua
+++ standard_program/main.lua
@@ -1,25 +1,33 @@
 require "move"
 require "receive_shell"
 
+
 main = {
-	LuaGpio:ReadyGpio()
+	LuaGpio:ReadyGpio(),
 	get = {}
 }
 
 function main.Catch(self)
-	repeat
-		while true do
-			LuaGpio:Left(6)
+
+	while true do
+
+		repeat
+			LuaGpio:Left(60)
 			self.get = Receive:ReadShell()
-		
-			if get[1]>0, get[2]>0, get[3]>0 do --
-				LuaGpio:Stop()
-				break
-			end
+		until self.get[2]>1 --get[2]：距離を予定
+
+		if self.get[2]<2 then
+			LuaGpio:Stop()
+			break
 		end
 
-		LuaGpio:Forward(5)
+		LuaGpio:Left(self.get[1]) --get[1]：相対角度を予定
 		LuaGpio:Stop()
 
-		until get[1]>0, get[2]>0, get[3]>0
+		LuaGpio:Forward(self.get[2])
+		LuaGpio:Stop()
+
+	end
 end
+
+main.run()
