c3d87a96f3d7dedc206923ca31a5b45e13edaae5
tubo-tubo <1130131@stu.hus.ac.jp>
2015-10-05 18:08:01 +0900

    kousin

--- standard_program/receive_shell_Test.lua
+++ standard_program/receive_shell_Test.lua
@@ -8,7 +8,17 @@ os.execute([[
 open = Receive:ReadShell()
 assert(open,5,6,4)
 
-print("Test is succeed!")
+print("Test1 is succeed!")
+
+os.execute([[
+			touch hoge.sh
+			echo x="5" y="6" z="4" > hoge.sh
+			]])
+
+open = Receive:ReadShell()
+assert(open,5,6,4)
+
+print("Test2 is succeed!")
 --open = Receive.new("test")
 --open:ReadShell()
 --assert(open:ReadShell(),"a",5)
