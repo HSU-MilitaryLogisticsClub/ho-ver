27cbb6bd6e88cd09fb529cb9097aaa5441ea4ab5
tubo-tubo <1130131@stu.hus.ac.jp>
2015-10-06 18:09:59 +0900

    3

--- standard_program/main.lua
+++ standard_program/main.lua
@@ -1,14 +1,15 @@
 require "move" --GPIOを使用するためのもの
-require "receive_shell" --シェル関数でファイルからデータを取るもの
+require "receive" --シェルファイルからデータを取るもの
 require "sleep" --いわゆる wait を使うためのもの
 
 main = {
 	LuaGpio:ReadyGpio()
 	get = {}
+	read = {}
 }
 
 function main.Catch(self)
-	self.get = Receive:ReadShell()
+	self.get = Receive.read(hoge)
 	a=1	
 
 	repeat
@@ -16,25 +17,30 @@ function main.Catch(self)
 		while true do	
 			if self.get[a]==1 then --luaの列は1から数える
 				while self.get[a]==1
-					self.LuaGpio:Left(1)
+					self.LuaGpio.Left(1)
 					a++
 					wait(0.1)
 
-					self.LuaGpio:Right(2)
+					self.LuaGpio.Right(2)
 					break
 				end
 
 			else
-				self.LuaGpio:Left(2)
-				
+				self.LuaGpio.Left(2)
+
 			end
 
 			wait(0.1)
 			a++
 		end
 
-		LuaGpio:Forward(3)
-		LuaGpio:Stop()
+		LuaGpio.Forward(3)
+		LuaGpio.Stop()
 
 	until get[a-1]==0
+end
+
+function main.Read(self)
+	self.read = Receive.ReadShell() --カメラ以外のセンサデータを取得
+
 end
