af5d17e408d08970bc5ef9d08434dfd51e74c6bb
tubo-tubo <1130131@stu.hus.ac.jp>
2015-11-25 19:01:33 +0900

    repair

--- standard_program/move_Test.lua
+++ standard_program/move_Test.lua
@@ -1,24 +1,21 @@
 require "move"
 
 function TestGpio()
-<<<<<<< HEAD
-=======
 	LuaGpio:ReadyGpio()
->>>>>>> 4076b72705b45c2e0feb5e35402811bb18f74b4f
-	LuaGpio:Forward(3)
+	LuaGpio.Forward(3)
+	LuaGpio.Stop()
+	os.execute("sleep 1s")
+
+	LuaGpio.Back(3)
 	LuaGpio:Stop()
 	os.execute("sleep 1s")
 
-	LuaGpio:Back(3)
-	LuaGpio:Stop()
+	LuaGpio.Left(3)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
-	LuaGpio:Left(3)
-	LuaGpio:Stop()
-	os.execute("sleep 1s")
-
-	LuaGpio:Right(3)
-	LuaGpio:Stop()
+	LuaGpio.Right(3)
+	LuaGpio.Stop()
 	os.execute("sleep 1s")
 
 end
