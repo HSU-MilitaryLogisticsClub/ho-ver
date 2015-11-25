9bc9fa5158f8870dfee00b01841b971933958a61
tubo-tubo <1130131@stu.hus.ac.jp>
2015-11-25 17:43:26 +0900

    add

---
+++ standard_program/hoge.py
@@ -0,0 +1,8 @@
+#config: utf-8
+
+def test():
+	print("Test2 begin\n")
+	print(5)
+
+if __name__ == "__main__":
+	test()
---
+++ standard_program/hoge.rb
@@ -0,0 +1,8 @@
+def test()
+	print("Test1 begin\n")
+	print(5,6,4)
+end
+
+if $0 == __FILE__
+	test()
+end
