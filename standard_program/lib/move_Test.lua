510dfb2876b3a751104e9e61cad6def448b27326
tubo-tubo <1130131@stu.hus.ac.jp>
2016-01-05 18:35:51 +0900

    shape up

--- standard_program/lib/move_Test.lua
+++ standard_program/lib/move_Test.lua
@@ -5,21 +5,26 @@ function TestGpio()
 
 	LuaGpio.Forward(3)
 	LuaGpio.Stop(0.1)
+	print("Forward")
 	os.execute("sleep 1s")
 
 	LuaGpio.Back(3)
 	LuaGpio.Stop(0.1)
+	print("Back")
 	os.execute("sleep 1s")
 
 	LuaGpio.Left(3)
 	LuaGpio.Stop(0.1)
+	print("Left")
 	os.execute("sleep 1s")
 
 	LuaGpio.Right(3)
 	LuaGpio.Stop(0.1)
+	print("Right")
 	os.execute("sleep 1s")
 
 	LuaGpio.Clean()
+	print("Finish!")
 end
 
 TestGpio()
