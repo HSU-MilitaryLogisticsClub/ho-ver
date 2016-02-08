161d06d7f1d9c9d9d27830fe7b6f4fc54e34089a
tubo-tubo <1130131@stu.hus.ac.jp>
2016-02-08 17:30:47 +0900

    Update move.lua

--- standard_program/lib/move.lua
+++ standard_program/lib/move.lua
@@ -5,7 +5,7 @@ LuaGpio = {
 leftmotor={27, 22}, --select RaspberryPi's GPIO pin to move left motor
 rightmotor={6, 5}, --select RaspberryPi's GPIO pin to move right motor
 }
-
+--[[
 function loop(run,left1,left2,right1,right2)
 	now = os.time()
 	repeat
@@ -15,6 +15,7 @@ function loop(run,left1,left2,right1,right2)
 		GPIO.output(LuaGpio.rightmotor[2], right2)
 	until (os.time() - now) >= run
 end
+]]
 
 function LuaGpio.ReadyGpio()
 	GPIO.setwarnings(False)
@@ -26,23 +27,58 @@ function LuaGpio.ReadyGpio()
 end
 
 function LuaGpio.Forward(runtime)
-	loop(runtime,GPIO.HIGH,GPIO.LOW,GPIO.HIGH,GPIO.LOW)
+--	loop(runtime,GPIO.HIGH,GPIO.LOW,GPIO.HIGH,GPIO.LOW)
+	now = os.time()
+	repeat
+		GPIO.output(LuaGpio.leftmotor[1], GPIO.HIGH)
+		GPIO.output(LuaGpio.leftmotor[2], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[1], GPIO.HIGH)
+		GPIO.output(LuaGpio.rightmotor[2], GPIO.LOW)
+	until (os.time() - now) >= runtime
 end
 
 function LuaGpio.Back(runtime)
-	loop(runtime,GPIO.LOW,GPIO.HIGH,GPIO.LOW,GPIO.HIGH)
+--	loop(runtime,GPIO.LOW,GPIO.HIGH,GPIO.LOW,GPIO.HIGH)
+	now = os.time()
+	repeat
+		GPIO.output(LuaGpio.leftmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.leftmotor[2], GPIO.HIGH)
+		GPIO.output(LuaGpio.rightmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[2], GPIO.HIGH)
+	until (os.time() - now) >= runtime
 end
 
 function LuaGpio.Left(runtime)
-	loop(runtime,GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH)
+--	loop(runtime,GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH)
+	now = os.time()
+	repeat
+		GPIO.output(LuaGpio.leftmotor[1], GPIO.HIGH)
+		GPIO.output(LuaGpio.leftmotor[2], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[2], GPIO.HIGH)
+	until (os.time() - now) >= runtime
 end
 
 function LuaGpio.Right(runtime)
-	loop(runtime,GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW)
+--	loop(runtime,GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW)
+	now = os.time()
+	repeat
+		GPIO.output(LuaGpio.leftmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.leftmotor[2], GPIO.HIGH)
+		GPIO.output(LuaGpio.rightmotor[1], GPIO.HIGH)
+		GPIO.output(LuaGpio.rightmotor[2], GPIO.LOW)
+	until (os.time() - now) >= runtime
 end
 
 function LuaGpio.Stop(runtime)
-	loop(runtime,GPIO.LOW,GPIO.LOW,GPIO.LOW,GPIO.LOW)
+--	loop(runtime,GPIO.LOW,GPIO.LOW,GPIO.LOW,GPIO.LOW)
+	now = os.time()
+	repeat
+		GPIO.output(LuaGpio.leftmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.leftmotor[2], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[1], GPIO.LOW)
+		GPIO.output(LuaGpio.rightmotor[2], GPIO.LOW)
+	until (os.time() - now) >= runtime
 end
 
 function LuaGpio.Clean()
