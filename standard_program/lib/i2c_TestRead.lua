dbd0a5f95e797b8c2d375ab569a7389ef82b9223
tubo-tubo <1130131@stu.hus.ac.jp>
2016-02-19 16:52:09 +0900

    Update i2c_TestRead.lua

--- standard_program/lib/i2c_TestRead.lua
+++ standard_program/lib/i2c_TestRead.lua
@@ -2,11 +2,14 @@ require "receive_shell"
 
 function i2cTest()
 	Receive.WriteDate()
-	now = os.time()
-	while timeCount == 10 do
+	print("write today's date")
+--	now = os.time()
+--	while timeCount == 10 do
 		Receive.CallShell()
-		timeCount = now - os.time
-	end
+--		timeCount = now - os.time
+--		print(print(timeCount)
+--	end
+	print("write censor's data")
 end
 
 i2cTest()
