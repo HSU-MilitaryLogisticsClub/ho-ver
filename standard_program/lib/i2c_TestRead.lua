6ab00fbe49425448dc582c7fb91953e1f561a7d4
tubo-tubo <1130131@stu.hus.ac.jp>
2016-02-19 15:50:36 +0900

    Update i2c_TestRead.lua

--- standard_program/lib/i2c_TestRead.lua
+++ standard_program/lib/i2c_TestRead.lua
@@ -1,7 +1,11 @@
 require "receive_shell"
 
-now = os.time()
-while timeCount == 10 do
-	Receive.CallShell()
-	timeCount = now - os.time
+function i2cTest()
+	now = os.time()
+	while timeCount == 10 do
+		Receive.CallShell()
+		timeCount = now - os.time
+	end
 end
+
+i2cTest()
